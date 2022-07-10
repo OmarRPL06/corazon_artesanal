5d71e3ee3aa2951bf2b140e03cae84b91452ecb8
Javi Perez <javi9876789@gmail.com>
2022-07-09 19:08:30 -0500

    Navbar aliniado a la derecha

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -12,38 +12,39 @@
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a href="{{ url("/") }}"><img src="/imagenes/logo.png" alt="" width="72" height="40"></a>
-        <div class="container-fluid">
-            <a class="navbar-brand" href="{{ url("/") }}">Pedidos UTS</a>
-            <div class="d-flex">
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                                data-bs-toggle="dropdown" aria-expanded="false">
-                                Cliente +
-                            </a>
-                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <li><a class="dropdown-item" href="#">Registrar</a></li>
-                                <li><a class="dropdown-item" href="#">Consultar</a></li>
-                            </ul>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Blog</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Servicios</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Nosotros</a>
-                        </li>
-                    </ul>
-                </div>
-                <!-- <form class="d-flex">
-                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                    <button class="btn btn-outline-success" type="submit">Search</button>
-                </form> -->
+    <nav class="navbar navbar-expand-lg bg-light">
+        <a class="navbar-brand" href="#">NAHUTEC</a>
+        <div class="container-fluid conatiner-lg-6">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-end" id="navbarTogglerDemo01">
+                <ul class="navbar-nav mr-auto mb-2 mb-lg-0">
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Categoria
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item" href="#">Ropa</a></li>
+                            <li><a class="dropdown-item" href="#">Blusa</a></li>
+                            <li><a class="dropdown-item" href="#">Sudaderas</a></li>
+                        </ul>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Ofertas</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link">Moda</a>
+                    </li>
+                    <li class="nav-item ml-auto">
+                        <a class="nav-link">Ayuda</a>
+                    </li>
+                </ul>
+                <form class="d-flex mb-0 h1" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Buscar productos y más" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Buscar</button>
+                </form>
             </div>
         </div>
     </nav>
