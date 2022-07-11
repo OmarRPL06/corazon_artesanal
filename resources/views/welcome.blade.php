21e849d18b6016ed032020960dedd619c9dae04d
OmarRPL06 <omarperezrl38@gmail.com>
2022-07-10 23:57:59 -0500

    Agregué una carta centrada en el body de Welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,11 +1,19 @@
 @extends('layouts.base')
 @section('contenido')
-
     <div class="container-tittle">
         <h3>BIENVENIDOS</h3>
         <h3>Javier Mejorará el Navbar</h3>
         <h3>y</h3>
         <h3>Jaime Mejorará el Footer</h3>
+    </div>
+
+    <div class="container-card-orpl">
+        <div class="card" style="width: 18rem;">
+            <img src="{{ asset('/imagenes/ejemplo.png') }}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <p class="card-text">Ejemplo de como mostrar imagen en laravel</p>
+            </div>
+        </div>
     </div>
 
     @include('includes.footer')
