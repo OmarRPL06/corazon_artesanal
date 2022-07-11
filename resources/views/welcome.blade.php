6f07318fecbcd3ba2a35539149d99f7b13e7226c
JaimeNA03 <jaime.na97@hotmail.com>
2022-07-11 00:05:41 -0500

    Agrege una carta mas al body en welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,6 +15,14 @@
             </div>
         </div>
     </div>
+    <div class="container-card-orpl">
+        <div class="card" style="width: 18rem;">
+            <img src="{{ asset('/imagenes/ejemplo.png') }}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <p class="card-text">Ejemplo de como mostrar imagen en laravel</p>
+            </div>
+        </div>
+    </div>
 
     @include('includes.footer')
 @endsection
